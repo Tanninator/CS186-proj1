4697440a19a9349d5460460302c7f12dae97576d
Tanninator <btan@Brians-MacBook-Pro-2.local>
2014-02-10 17:18:46 -0800

    owww

--- src/java/simpledb/RecordId.java
+++ src/java/simpledb/RecordId.java
@@ -10,6 +10,9 @@ public class RecordId implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    PageId pageId;
+    int tupleNum;
+    
     /**
      * Creates a new RecordId referring to the specified PageId and tuple
      * number.
@@ -20,23 +23,22 @@ public class RecordId implements Serializable {
      *            the tuple number within the page.
      */
     public RecordId(PageId pid, int tupleno) {
-        // some code goes here
+        pageId = pid;
+        tupleNum = tupleno;
     }
 
     /**
      * @return the tuple number this RecordId references.
      */
     public int tupleno() {
-        // some code goes here
-        return 0;
+        return tupleNum;
     }
 
     /**
      * @return the page id this RecordId references.
      */
     public PageId getPageId() {
-        // some code goes here
-        return null;
+        return pageId;
     }
 
     /**
@@ -47,8 +49,10 @@ public class RecordId implements Serializable {
      */
     @Override
     public boolean equals(Object o) {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        if (!(o instanceof RecordId)) {
+        	return false;
+        }
+        return ((getPageId() == ((RecordId)o).getPageId() && tupleno() == ((RecordId)o).tupleno()));
     }
 
     /**
